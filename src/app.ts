f38e2b60c777ac184d53ab11ce31c248bee0dc92
Kartik Patel <kartikpatel0170@gmail.com>
2023-05-05 10:28:32 -0600

    added create user API

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from 'express';
-require('dotenv').config();
+import dotenv from "dotenv";
+dotenv.config();
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
+import routes from "./routes";
 
 
 
 const app = express();
+app.use(express.json());
 
 const port = config.get<number>("port");
 app.listen(port, async () => {
     logger.info(`Server listening on port ${port}`);
    
     await connect();
+    routes(app);
 });
