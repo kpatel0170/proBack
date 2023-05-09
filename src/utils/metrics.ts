083a7207480dac3c4806da2d071f3e6281758c9b
Kartik Patel <kartikpatel0170@gmail.com>
2023-05-09 13:23:00 -0600

    added metrics for API and database stats

---
+++ src/utils/metrics.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import client from "prom-client";
+import log from "./logger";
+import dotenv from "dotenv";
+dotenv.config();
+import config from "config";
+
+const app = express();
+
+export const restResponseTimeHistogram = new client.Histogram({
+  name: "rest_response_time_duration_seconds",
+  help: "REST API response time in seconds",
+  labelNames: ["method", "route", "status_code"],
+});
+
+export const databaseResponseTimeHistogram = new client.Histogram({
+  name: "db_response_time_duration_seconds",
+  help: "Database response time in seconds",
+  labelNames: ["operation", "success"],
+});
+
+export function startMetricsServer() {
+  const collectDefaultMetrics = client.collectDefaultMetrics;
+
+  collectDefaultMetrics();
+
+  app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+
+    return res.send(await client.register.metrics());
+  });
+
+  const MetricsPORT = config.get<number>("MetricsPORT");
+  app.listen(MetricsPORT, () => {
+    log.info(`Metrics server started at ${MetricsPORT}`);
+  });
+}
